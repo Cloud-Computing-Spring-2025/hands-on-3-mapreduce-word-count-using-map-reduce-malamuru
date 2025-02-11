2722e967c0a1c572ed7908f5d2ce3d2c50443002
malamuru <malamuru@uncc.edu>
2025-02-11 19:01:49 +0000

    Mounikaoutputhandson3

--- src/main/java/com/example/WordMapper.java
+++ src/main/java/com/example/WordMapper.java
@@ -1,5 +1,21 @@
 package com.example;
 
-public class WordMapper{
-    
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken().replaceAll("[^a-zA-Z]", "").toLowerCase());
+            context.write(word, one);
+        }
+    }
 }
